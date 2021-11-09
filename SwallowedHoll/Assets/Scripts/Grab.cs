0dda274741b95ed02bf1000f6a698ca22f0546ab
Travp333 <72285478+Travp333@users.noreply.github.com>
2021-11-08 19:27:20 -0800

    Cleaned up Grab a bit, also added a third camera that only sees props to prevent your hands clipping through help objects

--- SwallowedHoll/Assets/Scripts/Grab.cs
+++ SwallowedHoll/Assets/Scripts/Grab.cs
@@ -63,17 +63,20 @@ public class Grab : MonoBehaviour
         justThrew = false;
     }
 
-    public void pickUp(GameObject origin, Transform dummy, Transform prop, Rigidbody propRB, GameObject[] balls, RaycastHit hit){
-        if(hit.transform.gameObject.GetComponent<objectSize>().sizes == objectSize.objectSizes.large){
+    public void pickUp(GameObject origin, Transform dummy, RaycastHit hit){
+        Transform prop = hit.transform;
+        Rigidbody propRB = hit.rigidbody;
+        GameObject propGame = hit.transform.gameObject;
+        if(propGame.GetComponent<objectSize>().sizes == objectSize.objectSizes.large){
             sizes = objectSizes.large;
         }
-        if(hit.transform.gameObject.GetComponent<objectSize>().sizes == objectSize.objectSizes.medium){
+        if(propGame.GetComponent<objectSize>().sizes == objectSize.objectSizes.medium){
             sizes = objectSizes.medium;
         }
-        if(hit.transform.gameObject.GetComponent<objectSize>().sizes == objectSize.objectSizes.small){
+        if(propGame.GetComponent<objectSize>().sizes == objectSize.objectSizes.small){
             sizes = objectSizes.small;                
         }     
-        if(hit.transform.gameObject.GetComponent<objectSize>().sizes == objectSize.objectSizes.tiny){
+        if(propGame.GetComponent<objectSize>().sizes == objectSize.objectSizes.tiny){
             sizes = objectSizes.tiny;
         }                   
         //disable dynamic bones
@@ -81,28 +84,22 @@ public class Grab : MonoBehaviour
         //trigger animation
         hand.setisHolding(true);
         // move the hit object to the grab point
-        hit.transform.position = dummy.transform.position;
+        prop.position = dummy.transform.position;
         // set the hit object to be a child of the grab point
-        hit.transform.SetParent(dummy);
+        prop.SetParent(dummy);
         // get a reference to the custom gravity rigidbody to disable gravity and sleeping
         propRB.isKinematic=(true);
         isHolding = true;
         // set the held object to the "nocollidewithplayer" layer to prevent clipping with the player
-        prop.transform.gameObject.layer = 16;
+        propGame.layer = 16;
         // do the same for all children and childrens children 
-        foreach ( Transform child in prop.transform){
+        foreach ( Transform child in prop){
             child.transform.gameObject.layer = 16;
             foreach ( Transform child2 in child.transform){
                 child2.transform.gameObject.layer = 16;
             }
-        // find if you grabbed a basketball. If so, disable it's "thruHoop" status
         }
-        foreach(GameObject b in balls){
-            if (b.gameObject == hit.transform.gameObject){
-                b.gameObject.GetComponent<BBall>().setThruHoop(false);
-                break;
-            }
-        }
+
     }
     void Update()
     {   //THROW
