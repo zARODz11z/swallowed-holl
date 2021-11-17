a11263db2275ba278316778c00b449340a29e0bc
Travp333 <72285478+Travp333@users.noreply.github.com>
2021-11-16 16:59:25 -0800

    Made food respawn after a timer once you eat it

--- SwallowedHoll/Assets/Scripts/Eat.cs
+++ SwallowedHoll/Assets/Scripts/Eat.cs
@@ -9,6 +9,16 @@ public class Eat : MonoBehaviour
     float food = default;
 
     //Eats the object, destroying it and adding the appropriate value to player's hunger
+    public void respawnFood(){
+        this.GetComponent<Renderer>().enabled = true;
+        this.GetComponent<BoxCollider>().enabled = true;
+    }
+    public void hideFood(Collider other){
+        other.transform.root.GetComponent<playerHunger>().increaseHunger(food);
+        this.GetComponent<Renderer>().enabled = false;
+        this.GetComponent<BoxCollider>().enabled = false;
+        Invoke("respawnFood", 5f);
+    }
     public void eatFood()
     {
         GetComponentInParent<playerHunger>().increaseHunger(food);
@@ -17,13 +27,11 @@ public class Eat : MonoBehaviour
 
     //If the food item is a floating object instead of a physical one, consume on contact
     void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player"){
+        if (other.tag == "Player" &&( ((other.transform.root.GetComponent<PlayerStats>().hunger + food) < 100) || other.gameObject.transform.root.gameObject.GetComponent<Movement>().Diving)){
             //Call the player's hungerDive() method to see if they should bounce on contant
             other.gameObject.transform.root.gameObject.GetComponent<Movement>().hungerDive();
             //Increase the player's hunger value
-            other.gameObject.transform.root.gameObject.GetComponent<playerHunger>().increaseHunger(food);    
-            //Destroy the food object
-            Destroy(gameObject);
+            hideFood(other);
 
         }
         
